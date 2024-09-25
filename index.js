e3f34663251c619f80cc3e137a3f5b4ad103c480
Jayantilal Prajapat <jayanti.prajapat@gmail.com>
2024-09-25 12:23:48 +0530

    added

---
+++ index.js
@@ -0,0 +1,28 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'sureshapp7994@gmail.com',
+        pass: 'lbsd wfte ehrh qjqj'
+    },
+    type: 'smtp', // Add the type property
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+});
+
+var mailOptions = {
+    from: 'suresh,p1898@gmail.com',
+    to: 'sureshapp7994+100@gmail.com,suresh.p1898@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
